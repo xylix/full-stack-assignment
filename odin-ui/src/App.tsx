3a5c3d19f5ebf380bddbbd690c416098dc760067
Kerkko Pelttari <kerkko@robocorp.com>
2023-06-04 18:34:08 +0300

    Complete step 1, add map displaying to odin-ui

--- odin-ui/src/App.tsx
+++ odin-ui/src/App.tsx
@@ -1,19 +1,34 @@
+import { useRef, useState, useEffect } from 'react';
+import mapboxgl from 'mapbox-gl'; // @ts-ignore eslint-disable-line import/no-webpack-loader-syntax
+import 'mapbox-gl/dist/mapbox-gl.css';
+
 import './App.css';
 
-const ErrorMessage = () => {
-  return (
-      <div className="error-message-container">
-        <h1 className="error-message">
-        ERROR 404 - ODIN NOT FOUND
-        </h1>
-      </div>
-  );
-};
+mapboxgl.accessToken = 'pk.eyJ1Ijoia3BlbHR0YXJpIiwiYSI6ImNsaWhrb21lZTBxNjUzZG53MWhzYXVtNTQifQ.CvzsjFfO4BMZ9JgDEI4Zdg';
 
 
 export const App = (): JSX.Element => {
+  const mapContainer: React.RefObject<HTMLDivElement>= useRef(null);
+  const map = useRef<mapboxgl.Map | null>(null);
+  const [lng, setLng] = useState(22.4673);
+  const [lat, setLat] = useState(59.8613);
+  const [zoom, setZoom] = useState(9);
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({ // @ts-ignore
+      container: mapContainer.current, // @ts-ignore
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [lng, lat],
+      zoom: zoom
+    });
+  });
   return (
-    <ErrorMessage/>
+    <div>
+      <h2>Odin</h2>
+      <div>
+        <div ref={mapContainer} className="map-container" />
+      </div>
+    </div>
   );
 }
 
