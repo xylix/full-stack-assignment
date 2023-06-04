6a04a7b8c3fc54a42d6493e5f56f948c3e9e226f
Kerkko Pelttari <kerkko@robocorp.com>
2023-06-04 20:43:27 +0300

    Complete step 2, add image overlay

--- odin-ui/src/App.tsx
+++ odin-ui/src/App.tsx
@@ -6,7 +6,6 @@ import './App.css';
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoia3BlbHR0YXJpIiwiYSI6ImNsaWhrb21lZTBxNjUzZG53MWhzYXVtNTQifQ.CvzsjFfO4BMZ9JgDEI4Zdg';
 
-
 export const App = (): JSX.Element => {
   const mapContainer: React.RefObject<HTMLDivElement>= useRef(null);
   const map = useRef<mapboxgl.Map | null>(null);
@@ -15,13 +14,32 @@ export const App = (): JSX.Element => {
   const [zoom, setZoom] = useState(9);
   useEffect(() => {
     if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({ // @ts-ignore
+    const newMap = new mapboxgl.Map({ // @ts-ignore
       container: mapContainer.current, // @ts-ignore
       style: 'mapbox://styles/mapbox/streets-v12',
       center: [lng, lat],
       zoom: zoom
     });
+    map.current = newMap
+    newMap.on('style.load', () => {
+        newMap.addSource("myImageSource", {"type": "image",
+      "url": "http://127.0.0.1:8000/sar_image",
+       "coordinates": [
+         [22.2908182629724, 59.91614254645401],
+         [22.578806773313246, 59.947751078236365],
+         [22.638044070378744, 59.809992490984754],
+         [22.351391574531174, 59.77847599974091],
+       ]})
+       newMap.addLayer({
+        "id": "overlay",
+        "source": "myImageSource",
+        "type": "raster",
+        "paint": {"raster-opacity": 0.85}
+      })
+    })
   });
+
+
   return (
     <div>
       <h2>Odin</h2>
