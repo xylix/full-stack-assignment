f2ec3962ed2f52147428d92c86a1d7776c79f083
Kerkko Pelttari <kerkko@robocorp.com>
2023-06-04 17:55:28 +0300

    Add image serving endpoint to backend

--- odin-api/main.py
+++ odin-api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import Response
 
 app = FastAPI()
 
@@ -6,3 +7,10 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return "ODIN-API NOT FOUND"
+
+@app.get("/sar_image")
+async def sar_image():
+    with open("./SAR_image_20420212.png", "rb") as img:
+        image_data = img.read()
+    return Response(content=image_data, media_type="image/png")
+
